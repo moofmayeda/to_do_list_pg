031ceeccc6789ce66b0467c4056dd0ea1415403a
moof mayeda & Eric Johnson <student@epicodus.com>
2014-08-04 11:10:13 -0700

    add DB save / clear :cat:

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,7 +1,30 @@
+require 'pg'
+
 class Task
   attr_accessor :name
 
   def initialize name
     @name = name
+  end
+
+  def self.all
+    tasks = []
+    results = DB.exec("SELECT * FROM tasks;")
+    results.each do |result|
+      name = result['name']
+      due_date = result['due_date']
+      list_id = result['list_id']
+      done = result['done']
+      tasks << Task.new(name)
+    end
+    tasks
+  end
+
+  def save
+    DB.exec("INSERT INTO tasks (name) VALUES ('#{name}');")
+  end
+
+  def == another_task
+    self.name == another_task.name
   end
 end
--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -2,10 +2,42 @@ require 'rspec'
 require 'pry'
 require 'tasks'
 
+DB = PG.connect({:dbname => 'test_todo'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
+
 describe 'Task' do
-  it 'initializes a new task with a name' do
-    new_task = Task.new('pull weeds')
-    expect(new_task).to be_an_instance_of Task
-    expect(new_task.name).to eq 'pull weeds'
+  describe 'initialize' do
+    it 'initializes a new task with a name' do
+      new_task = Task.new('pull weeds')
+      expect(new_task).to be_an_instance_of Task
+      expect(new_task.name).to eq 'pull weeds'
+    end
+  end
+
+  describe '.all' do
+    it 'starts as a blank array' do
+      expect(Task.all).to eq []
+    end
+  end
+
+  describe 'save' do
+    it 'saves a task' do
+      new_task = Task.new('pull weeds')
+      new_task.save
+      expect(Task.all[0]).to eq new_task
+    end
+  end
+
+  describe '==' do
+    it "sets two tasks as equal if they have the same name" do
+      new_task1 = Task.new('pull weeds')
+      new_task2 = Task.new('pull weeds')
+      expect(new_task1).to eq new_task2
+    end
   end
 end
